487386e615dc48ce6f971e7c49c9ea3e4baed736
lauratugee <laura.tugee@strathmre.edu>
2025-11-13 09:09:49 +0300

    fixed line

---
+++ app/src/main/java/com/example/churchmanagementsystem/repository/AuthRepository.kt
@@ -0,0 +1,21 @@
+package com.example.churchmanagementsystem.repository
+
+import com.example.churchmanagementsystem.api.RetrofitInstance
+import com.example.churchmanagementsystem.models.User
+import retrofit2.Response
+
+class AuthRepository {
+    suspend fun loginUser(credentials: Map<String, String>): Response<User> {
+        return RetrofitInstance.apiService.loginUser(credentials)
+    }
+
+    suspend fun registerUser(user: User): Response<User> {
+        return RetrofitInstance.apiService.registerUser(user)
+    }
+
+    suspend fun getUserbyId(userId: Int): Response<User> {
+        return RetrofitInstance.apiService.getUsersById(userId)
+    }
+
+
+}
