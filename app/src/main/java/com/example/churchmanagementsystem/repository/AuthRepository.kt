cd242e36e9fe6bd293f0e6b6a55819cf7ab59e8f
lauratugee <laura.tugee@strathmre.edu>
2025-11-13 21:11:03 +0300

    updated changes

--- app/src/main/java/com/example/churchmanagementsystem/repository/AuthRepository.kt
+++ app/src/main/java/com/example/churchmanagementsystem/repository/AuthRepository.kt
@@ -2,21 +2,24 @@ package com.example.churchmanagementsystem.repository
 
 import com.example.churchmanagementsystem.api.RetrofitInstance
 import com.example.churchmanagementsystem.models.User
-import retrofit2.Response
 
-class AuthRepository {
-    suspend fun loginUser(credentials: Map<String, String>): Response<User> {
-        return RetrofitInstance.api.loginUser(credentials)
-    }
+class AuthRepository{
+    private val apiService = RetrofitInstance.api
 
-    suspend fun registerUser(user:User, password: String): Response<User> {
-        val request= RegistrationRequest(user, password)
-        return RetrofitInstance.api.registerUser(request)
-    }
-
-    suspend fun getUserById(userId: Int): Response<User> {
-        return RetrofitInstance.api.getUserById(userId)
-    }
-
-
+    suspend fun registerUser(user: User, password: String) = apiService.registerUser(
+        mapOf(
+            "user" to user,
+            "password" to password
+        )
+    )
+    suspend fun loginUser(credentials: Map<String, String>) = apiService.loginUser(
+        mapOf(
+            "email" to credentials["email"]!!,
+            "password" to credentials["password"]!!
+        )
+    )
 }
+
+
+
+
