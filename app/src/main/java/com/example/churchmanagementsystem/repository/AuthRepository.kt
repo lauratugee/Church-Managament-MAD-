35bd79c6e20b5d3a27563f07f8d9f450e74c58fb
lauratugee <laura.tugee@strathmre.edu>
2025-11-13 09:21:47 +0300

    fixed line

--- app/src/main/java/com/example/churchmanagementsystem/repository/AuthRepository.kt
+++ app/src/main/java/com/example/churchmanagementsystem/repository/AuthRepository.kt
@@ -6,15 +6,15 @@ import retrofit2.Response
 
 class AuthRepository {
     suspend fun loginUser(credentials: Map<String, String>): Response<User> {
-        return RetrofitInstance.apiService.loginUser(credentials)
+        return RetrofitInstance.api.loginUser(credentials)
     }
 
     suspend fun registerUser(user: User): Response<User> {
-        return RetrofitInstance.apiService.registerUser(user)
+        return RetrofitInstance.api.registerUser(user)
     }
 
     suspend fun getUserById(userId: Int): Response<User> {
-        return RetrofitInstance.apiService.getUsersById(userId)
+        return RetrofitInstance.api.getUserById(userId)
     }
 
 
