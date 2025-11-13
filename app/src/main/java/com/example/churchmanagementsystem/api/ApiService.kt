0d8cfb6c70d8e1b7b653f17ac39a6ed5909d021e
lauratugee <laura.tugee@strathmre.edu>
2025-11-13 20:50:03 +0300

    updated changes

--- app/src/main/java/com/example/churchmanagementsystem/api/ApiService.kt
+++ app/src/main/java/com/example/churchmanagementsystem/api/ApiService.kt
@@ -1,21 +1,15 @@
 package com.example.churchmanagementsystem.api
 
-import com.example.churchmanagementsystem.models.RegistrationRequest
 import com.example.churchmanagementsystem.models.User
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
-import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface ApiService {
-    @POST("auth/login")
-    suspend fun loginUser(@Body credentials: Map<String,String>): Response<User>
-    @POST("auth/register")
-    suspend fun registerUser(@Body registrationRequest: RegistrationRequest):Response<User>
-    @GET("users")
-    suspend fun getAllUsers():Response<List<User>>
-    @GET("users/{id}")
-    suspend fun getUserById(@Path ("id") userId: Int):Response<User>
+    @POST("register")
+    suspend fun registerUser(@Body body: Map<String, @JvmSuppressWildcards Any>): Response<User>
+
+    @POST("login")
+    suspend fun loginUser(@Body credentials: Map<String, String>): Response<User>
 
 }
