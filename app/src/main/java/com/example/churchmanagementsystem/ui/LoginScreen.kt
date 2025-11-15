d254bc75ccb4648fb7f5bc16f1fd7e53162b4afa
lauratugee <laura.tugee@strathmre.edu>
2025-11-15 11:01:48 +0300

    updated lines

--- app/src/main/java/com/example/churchmanagementsystem/ui/LoginScreen.kt
+++ app/src/main/java/com/example/churchmanagementsystem/ui/LoginScreen.kt
@@ -1,15 +1,18 @@
 package com.example.churchmanagementsystem.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import com.example.churchmanagementsystem.viewmodel.AuthViewModel
 
 @Composable
@@ -23,24 +26,37 @@ fun LoginScreen(
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     val loginState by authViewModel.loginState.collectAsState()
+    val context= LocalContext.current
 
     LaunchedEffect(loginState) {
-        if (loginState is AuthViewModel.DataState.Success) {
-            val user = (loginState as AuthViewModel.DataState.Success).data
-
-            val destination = when {
-                user.role.equals("admin", ignoreCase = true) -> "admin_dashboard"
-                user.role.equals("treasurer", ignoreCase = true) -> "treasurer_dashboard"
-                else -> "home/${user.email}"
-            }
-            navController.navigate(destination) {
-                popUpTo("login") {
-                    inclusive = true
+        when (val state=loginState){
+            is AuthViewModel.DataState.Success -> {
+                Toast.makeText(context, "Login successful!", Toast.LENGTH_SHORT).show()
+                val user = state.data
+                val destination = when {
+                    user.role == "admin" -> "admin_dashboard"
+                    user.role == "treasurer" -> "treasurer_dashboard"
+                    else -> "home/${user.email}"
                 }
+                navController.navigate(destination) {
+                    popUpTo(navController.graph.startDestinationId) {
+                        inclusive = true
+                    }
+                }
+                authViewModel.logout()
+                }
+
+            is AuthViewModel.DataState.Error -> {
+                Toast.makeText(context, "Login failed: ${state.message}", Toast.LENGTH_SHORT).show()
+                authViewModel.logout()
             }
+            else -> {}
         }
     }
-    Surface(modifier = Modifier.fillMaxSize()) {
+
+
+
+    Box(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -51,6 +67,8 @@ fun LoginScreen(
         ) {
             Text("Login", style = MaterialTheme.typography.headlineMedium)
             Spacer(modifier = Modifier.height(32.dp))
+
+            val isUIEnabled=loginState !is AuthViewModel.DataState.Loading
 
 
             //Email
@@ -79,36 +97,25 @@ fun LoginScreen(
                     authViewModel.login(email, password)
                 },
                 modifier = Modifier.fillMaxWidth(),
-                enabled = loginState !is AuthViewModel.DataState.Loading && email.isNotEmpty() && password.isNotEmpty()
+                enabled = isUIEnabled && email.isNotEmpty()
             )
             {
                 Text("Login")
             }
             Spacer(modifier = Modifier.height(16.dp))
 
-            when (val state = loginState) {
-                is AuthViewModel.DataState.Loading -> {
-                    CircularProgressIndicator()
-                }
-
-                is AuthViewModel.DataState.Error -> {
-                    Text("Login failed: ${state.message}", color = MaterialTheme.colorScheme.error)
-                }
-
-                else -> {}
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-
-            TextButton(onClick = onNavigateToRegister) {
+            //Register
+            TextButton(onClick = onNavigateToRegister, enabled=isUIEnabled) {
                 Text("Don't have an account? Register here")
             }
-            TextButton(onClick = {
-                navController.navigate("forgot_password")
-            }) {
+            TextButton(
+                onClick = {},
+               enabled=isUIEnabled
+            ) {
                 Text("Forgot password?")
             }
-
-
         }
+        if (loginState is AuthViewModel.DataState.Loading)
+            CircularProgressIndicator(modifier=Modifier.align(Alignment.Center))
     }
-}
+    }
