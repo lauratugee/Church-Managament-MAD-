182356384706076a74b047287242f0ab7aff5381
lauratugee <laura.tugee@strathmre.edu>
2025-11-15 09:31:19 +0300

    updated lines

--- app/src/main/java/com/example/churchmanagementsystem/repository/AuthRepository.kt
+++ app/src/main/java/com/example/churchmanagementsystem/repository/AuthRepository.kt
@@ -1,26 +1,67 @@
 package com.example.churchmanagementsystem.repository
 
 
-import com.example.churchmanagementsystem.api.RetrofitInstance
+import com.example.churchmanagementsystem.api.ApiService
 import com.example.churchmanagementsystem.models.User
+import com.example.churchmanagementsystem.viewModels.AuthViewModel
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-class AuthRepository{
-    private val apiService = RetrofitInstance.api
 
+class AuthRepository(private val apiService: ApiService){
 
+    suspend fun register(
+        firstName: String,
+        lastName: String,
+        email: String,
+        dateOfBirth: String,
+        phoneNumber: String,
+        dateJoined: String,
+        gender: String,
+        maritalStatus: String
+    ): DataState<User> {
+        return try{
+            val dateJoined=SimpleDateFormat("dd-MM-yyyy",Locale.getDefault()).format(Date())
 
-    suspend fun register(user: User, password: String) = apiService.registerUser(
-    mapOf(
-        "user" to user,
-        "password" to password
-       )
-    )
-    suspend fun login(credentials: Map<String, String>) = apiService.loginUser(
-    mapOf(
-        "email" to credentials["email"]!!,
-        "password" to credentials["password"]!!
-      )
-    )
+            val userDetails=mapOf(
+                "firstName" to firstName,
+                "lastName" to lastName,
+                "email" to email,
+                "dateOfBirth" to dateOfBirth,
+                "phoneNumber" to phoneNumber,
+                "dateJoined" to dateJoined,
+                "gender" to gender,
+                "maritalStatus" to maritalStatus
+            )
+            val response=apiService.registerUser(userDetails)
+            if (response.isSuccessful && response.body()!=null){
+                DataState.Success(response.body()!!)
+            } else{
+                DataState.Error(response.message())
+
+            }
+        } catch (e: Exception){
+            DataState.Error(message ?: "Unknown error")
+        }
+    }
+
+    suspend fun login(email: String, password: String): DataState<User> {
+        return try {
+            val credentials = mapOf("email" to email, "password" to password)
+            val response = apiService.loginUser(credentials)
+
+            if (response.isSuccessful && response.body() != null) {
+                DataState.Success(response.body()!!)
+            } else{
+                DataState.Error("Error: ${response.code()}-${response.message()}")
+            }
+        } catch (e: Exception) {
+            DataState.Error("Error: ${e.message}")
+            }
+
+    }
+
 
 }
 
