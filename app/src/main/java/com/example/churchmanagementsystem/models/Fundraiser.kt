cbee38e94da5b2c13ea1d2b751ac199bf634a485
lauratugee <laura.tugee@strathmre.edu>
2025-11-16 21:26:30 +0300

    updated changes

--- app/src/main/java/com/example/churchmanagementsystem/models/Fundraiser.kt
+++ app/src/main/java/com/example/churchmanagementsystem/models/Fundraiser.kt
@@ -1,6 +1,12 @@
 package com.example.churchmanagementsystem.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "fundraisers")
+
 data class Fundraiser (
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val name: String,
     val amount: Double,
--- app/src/main/java/com/example/churchmanagementsystem/models/Tithe.kt
+++ app/src/main/java/com/example/churchmanagementsystem/models/Tithe.kt
@@ -1,11 +1,17 @@
 package com.example.churchmanagementsystem.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tithes")
+
 data class Tithe (
+    @PrimaryKey(autoGenerate = true)
     val id:Int,
-    val massName: String?,
+    val massName: String,
     val amount: Double,
-    val date: String?,
-    val notes: String?
+    val date: String,
+    val notes: String
 )
 
 
