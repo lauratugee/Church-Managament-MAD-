10d1625a8134e1c8bc6334d455ea3149abaf1f57
lauratugee <laura.tugee@strathmre.edu>
2025-11-16 17:22:24 +0300

    trasurer repo created

---
+++ app/src/main/java/com/example/churchmanagementsystem/repository/TreasurerRepository.kt
@@ -0,0 +1,6 @@
+package com.example.churchmanagementsystem.repository
+
+import com.example.churchmanagementsystem.api.ApiService
+
+class TreasurerRepository (private val apiService: ApiService){
+}
