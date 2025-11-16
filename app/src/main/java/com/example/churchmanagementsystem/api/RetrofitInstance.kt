73bc8d55b355df6d20807863946f518c39980356
lauratugee <laura.tugee@strathmre.edu>
2025-11-16 23:51:17 +0300

    updated changes

--- app/src/main/java/com/example/churchmanagementsystem/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/churchmanagementsystem/api/RetrofitInstance.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL= "http://10.0.2.2:3000/api"
+    private const val BASE_URL= "http://10.0.2.2:3000/"
     private val loggingInterceptor = HttpLoggingInterceptor().apply{
         level= HttpLoggingInterceptor.Level.BODY
     }
