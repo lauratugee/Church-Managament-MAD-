b89d0f7472fd4f817e57841d03feeccb6772e8d7
lauratugee <laura.tugee@strathmre.edu>
2025-11-16 13:42:06 +0300

    changes

--- app/src/main/java/com/example/churchmanagementsystem/util/DataState.kt
+++ app/src/main/java/com/example/churchmanagementsystem/util/DataState.kt
@@ -1,7 +1,7 @@
 package com.example.churchmanagementsystem.util
 
-sealed class DataState {
-    data class Success<T>(val data: T) : DataState()
+sealed class DataState<out T> {
+    data class Success<out T>(val data: T) : DataState<T>()
     data class Error(val message: String) : DataState<Nothing>()
     object Loading : DataState<Nothing>()
     object Idle : DataState<Nothing>()
