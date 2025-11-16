e5df27eb7940553b094878f5b7e7bfeb588555d0
lauratugee <laura.tugee@strathmre.edu>
2025-11-16 13:43:46 +0300

    changes

--- app/src/main/java/com/example/churchmanagementsystem/ui/RegistrationScreen.kt
+++ app/src/main/java/com/example/churchmanagementsystem/ui/RegistrationScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import  androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
