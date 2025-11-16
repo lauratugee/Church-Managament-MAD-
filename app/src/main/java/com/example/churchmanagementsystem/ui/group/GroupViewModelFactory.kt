8cb0618613959c5ea92c82518c8d7cf4a38d8d60
lauratugee <laura.tugee@strathmre.edu>
2025-11-16 09:50:48 +0300

    new GroupViewModelFactory.kt

---
+++ app/src/main/java/com/example/churchmanagementsystem/ui/group/GroupViewModelFactory.kt
@@ -0,0 +1,19 @@
+package com.example.churchmanagementsystem.ui.group
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.churchmanagementsystem.data.GroupRepository
+
+
+class GroupViewModelFactory (private val groupRepository: GroupRepository): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(GroupViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return GroupViewModel(groupRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+
+
+}
